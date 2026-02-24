7eb2c08935a3c578ff097493216da644c5e2c683
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2026-02-23 23:24:13 -0500

    Implement team color selection and application in MainManager and Unit scripts

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -4,10 +4,17 @@ public class MainManager : MonoBehaviour
 {
     public static MainManager Instance;
 
+    public Color TeamColor;
+
     private void Awake()
     {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
         Instance = this;
         DontDestroyOnLoad(gameObject);
-        
     }
 }
