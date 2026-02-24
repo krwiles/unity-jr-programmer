67458f00687a1fd38ab0c1849d76fb31267e56a0
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2026-02-23 21:29:06 -0500

    Update Universal Render Pipeline and Project Settings
    - Incremented asset versions in UniversalRP-HighQuality and UniversalRenderPipelineGlobalSettings.
    - Added new reflection probe settings and updated prefilter settings in UniversalRP-HighQuality.
    - Updated dependencies in manifest.json and packages-lock.json to latest versions.
    - Modified GraphicsSettings to include shader build settings.
    - Updated ProjectSettings with new serialized version and additional player settings for Android and iOS.
    - Updated ProjectVersion to reflect the new editor version.
    - Added a new solution file for the project.

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+    }
+}
